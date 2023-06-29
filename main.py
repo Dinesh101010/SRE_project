9a9e23061a2c2fe804d0552b4f5fe06248468e5b
Dinesh <dineshbhogadi@gmail.com>
2023-06-29 14:12:17 -0500

    Committing the code with comments

--- main.py
+++ main.py
@@ -4,21 +4,23 @@ import yaml
 from collections import defaultdict
 
 
+# Function to check the health of an endpoint
 def check_health(endpoint):
     method = endpoint.get('method', 'GET')
     url = endpoint['url']
     headers = endpoint.get('headers', {})
     body = endpoint.get('body')
-
+    # Send an HTTP request to the endpoint
     response = requests.request(method, url, headers=headers, json=body)
     latency = response.elapsed.total_seconds() * 1000
-
+    # Determine if the endpoint is UP or DOWN based on response code and latency
     if 200 <= response.status_code < 300 and latency < 500:
         return 'UP', latency
     else:
         return 'DOWN', latency
 
 
+# Function to log the availability percentage for each domain
 def log_availability(availability):
     for domain, status_count in availability.items():
         total = sum(status_count.values())
@@ -27,20 +29,25 @@ def log_availability(availability):
         print(f"{domain} has {percentage}% availability percentage")
 
 
+# Main function to run the health checks
 def main(config_file):
     with open(config_file) as f:
         endpoints = yaml.safe_load(f)
-
+    # Use a defaultdict to store availability counts for each domain
     availability = defaultdict(lambda: defaultdict(int))
 
     try:
         while True:
+            # Iterate over each endpoint and check its health
             for endpoint in endpoints:
+                # Extract the domain from the URL
                 domain = endpoint['url'].split('/')[2]
+                # Check the health of the endpoint
                 status, _ = check_health(endpoint)
                 availability[domain][status] += 1
-
+            # Log the availability percentage
             log_availability(availability)
+            # Wait for 15 seconds before the next cycle
             time.sleep(15)
 
     except KeyboardInterrupt:
